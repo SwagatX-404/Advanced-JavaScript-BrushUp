902673bfade12097d32746915dbf73d2d4e0137c
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-10-06 23:59:46 +0530

    basics

--- variables_datatypes.js
+++ variables_datatypes.js
@@ -12,6 +12,21 @@
 
             console.log(name)
             console.log(greeting)
+
+            //Numbers
+            let age = 23
+            const PI = 3.14159
+
+            console.log(age,"       ",PI)
+
+            // Boolean
+            let isStudent = true;
+            const hasJob = false;
+            
+
+            //Undefined an Null
+            let undefinedVar;
+            let emptyValue = null;
         }
 
         exampleVariable();
